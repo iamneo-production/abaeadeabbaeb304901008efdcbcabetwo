32f2f004cbe22e98458f1f52d21b882e0be9c8b6
Iamneo <techadmin@iamneo.ai>
2023-09-10 07:40:45 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -221,7 +221,111 @@ function myfunc(){
             document.getElementById('result-container').innerHTML="Player X Turn";
         }
         else{
-            document.getElementById('result-container').innerHTML=
+            document.getElementById('result-container').innerHTML="Player O Turn";
         }
+    }
+}
+function myfunc_3(){
+    if(flag==1){
+        document.getElementById("b1").value="X";
+        document.getElementById("b1").disabled=true;
+        flag=0;
+    }
+    else{
+        document.getElementById("b1").value="O";
+        document.getElementById("b1").disabled=true;
+        flag=1;
+
+    }
+}
+function myfunc_4(){
+    if(flag==1){
+        document.getElementById("b2").value="X";
+        document.getElementById("b2").disabled=true;
+        flag=0;
+    }
+    else{
+        document.getElementById("b2").value="O";
+        document.getElementById("b2").disabled=true;
+        flag=1;
+
+    }
+}
+function myfunc_5(){
+    if(flag==1){
+        document.getElementById("b3").value="X";
+        document.getElementById("b3").disabled=true;
+        flag=0;
+    }
+    else{
+        document.getElementById("b3").value="O";
+        document.getElementById("b3").disabled=true;
+        flag=1;
+
+    }
+}
+function myfunc_6(){
+    if(flag==1){
+        document.getElementById("b4").value="X";
+        document.getElementById("b4").disabled=true;
+        flag=0;
+    }
+    else{
+        document.getElementById("b4").value="O";
+        document.getElementById("b4").disabled=true;
+        flag=1;
+
+    }
+}
+function myfunc_7(){
+    if(flag==1){
+        document.getElementById("b5").value="X";
+        document.getElementById("b5").disabled=true;
+        flag=0;
+    }
+    else{
+        document.getElementById("b5").value="O";
+        document.getElementById("b5").disabled=true;
+        flag=1;
+
+    }
+}
+function myfunc_8(){
+    if(flag==1){
+        document.getElementById("b6").value="X";
+        document.getElementById("b6").disabled=true;
+        flag=0;
+    }
+    else{
+        document.getElementById("b6").value="O";
+        document.getElementById("b6").disabled=true;
+        flag=1;
+
+    }
+}
+function myfunc_9(){
+    if(flag==1){
+        document.getElementById("b7").value="X";
+        document.getElementById("b7").disabled=true;
+        flag=0;
+    }
+    else{
+        document.getElementById("b7").value="O";
+        document.getElementById("b7").disabled=true;
+        flag=1;
+
+    }
+}
+function myfunc_3(){
+    if(flag==1){
+        document.getElementById("b1").value="X";
+        document.getElementById("b1").disabled=true;
+        flag=0;
+    }
+    else{
+        document.getElementById("b1").value="O";
+        document.getElementById("b1").disabled=true;
+        flag=1;
+
     }
 }
